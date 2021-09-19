a9039b020bece8276e7f6b308246d2425f8d59b4
js-bruno <brunocebr@hotmail.com>
2021-09-19 14:55:15 -0300

    Ajustando responses e services.

--- src/main/java/com/parking/api/controller/ParkingController.java
+++ src/main/java/com/parking/api/controller/ParkingController.java
@@ -1,7 +1,7 @@
 package com.parking.api.controller;
 
 import com.parking.api.model.Parking;
-import com.parking.api.repository.ParkingRepository;
+import com.parking.api.service.ParkingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,62 +20,31 @@ import java.util.Map;
 public class ParkingController {
 
     @Autowired
-    private ParkingRepository parkingRepository;
+    private ParkingService parkingService;
 
     @GetMapping("/")
     public List<Parking> listParkings() {
-        return parkingRepository.findAll();
+        return parkingService.findAll();
     }
 
     @GetMapping("/{plate}")
     public List<Parking> getParking(@PathVariable("plate") String plate) {
-        return parkingRepository.findByPlate(plate);
+        return parkingService.findByPlate(plate);
     }
 
     @PostMapping("/{plate}")
-    public String reserveParking(@PathVariable("plate") String plate) {
-        List<Parking> parkings = this.parkingRepository.findByPlate(plate);
-        if(!parkings.isEmpty()) {
-            Parking lastPark = parkings.get(parkings.size() - 1);
-            if (!lastPark.isOut()) {
-                return "Veiculo já esta estacionado";
-            }
-        }
-
-        long millis=System.currentTimeMillis();
-        Date dateEntry = new java.util.Date(millis);
-
-        Parking parking = new Parking(plate, "60 minutos", false, false, dateEntry, null);
-        this.parkingRepository.save(parking);
-//        return this.parkingRepository.save(parking).getId();
-        return "Veiculo Estacionado com sucesso";
+    public ResponseEntity<?> reserveParking(@PathVariable("plate") String plate) {
+        return this.parkingService.checkAndSave(plate);
     }
 
     @PatchMapping("/{plate}/pay")
     public Parking payParking(@PathVariable("plate") String plate) {
-        List<Parking> park=this.parkingRepository.findByPlate(plate);
-        Parking lastPark = park.get(park.size() - 1);
-        lastPark.setPaid(true);
-        this.parkingRepository.save(lastPark);
-        return lastPark;
+        return this.parkingService.checkAndUpdatePay(plate);
     }
 
     @PatchMapping("/{plate}/out")
-    public String outParking(@PathVariable("plate") String plate) {
-        List<Parking> park=this.parkingRepository.findByPlate(plate);
-        Parking lastPark = park.get(park.size() - 1);
-        if (lastPark.isPaid()) {
-            long millis=System.currentTimeMillis();
-            Date dateOut = new java.util.Date(millis);
-
-            lastPark.setOut(true);
-            lastPark.setDateTimeOut(dateOut);
-            this.parkingRepository.save(lastPark);
-            return "Muito Obrigado, Volte Sempre.";
-        }
-        else {
-            return "O Estacionamento Ainda Não Foi Pago";
-        }
+    public ResponseEntity<?> outParking(@PathVariable("plate") String plate) {
+        return this.parkingService.checkAndUpdateOut(plate);
 
     }
 
--- src/main/java/com/parking/api/service/ParkingService.java
+++ src/main/java/com/parking/api/service/ParkingService.java
@@ -3,12 +3,72 @@ package com.parking.api.service;
 import com.parking.api.model.Parking;
 import com.parking.api.repository.ParkingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class ParkingService {
     @Autowired
     ParkingRepository repository;
 
-//    public Parking findByPlate(String plate) {
-//        repository.findByPlate
-//    }
+    public List<Parking> findAll() {
+        return repository.findAll();
+    }
+
+    public List<Parking> findByPlate(String plate) {
+        List<Parking> parkins = repository.findByPlate(plate);
+        return parkins;
+    }
+
+    public void save(Parking park) {
+        repository.save(park);
+    }
+
+    public ResponseEntity<?> checkAndSave(String plate) {
+        List<Parking> parkings = this.findByPlate(plate);
+        if(!parkings.isEmpty()) {
+            Parking lastPark = parkings.get(parkings.size() - 1);
+            if (!lastPark.isOut()) {
+                return new ResponseEntity<>("Placa Já Esta Estacionada", HttpStatus.NOT_ACCEPTABLE);
+            }
+        }
+
+        long millis=System.currentTimeMillis();
+        Date dateEntry = new java.util.Date(millis);
+
+        Parking parking = new Parking(plate, "60 minutos", false, false, dateEntry, null);
+        this.save(parking);
+        return new ResponseEntity<>(parking, HttpStatus.CREATED);
+    }
+
+    public Parking checkAndUpdatePay(String plate) {
+        List<Parking> park=this.findByPlate(plate);
+        Parking lastPark = park.get(park.size() - 1);
+        lastPark.setPaid(true);
+        this.save(lastPark);
+        return lastPark;
+    }
+
+    public ResponseEntity<?> checkAndUpdateOut(String plate) {
+        List<Parking> park=this.findByPlate(plate);
+        Parking lastPark = park.get(park.size() - 1);
+        if (lastPark.isPaid()) {
+            long millis=System.currentTimeMillis();
+            Date dateOut = new java.util.Date(millis);
+
+            lastPark.setOut(true);
+            lastPark.setDateTimeOut(dateOut);
+            this.save(lastPark);
+            return new ResponseEntity<>(lastPark, HttpStatus.NO_CONTENT);
+        }
+        else {
+            return new ResponseEntity<>("Estacionamento ainda não foi pago", HttpStatus.NOT_MODIFIED);
+        }
+    }
+
+
 }
